b48313a55436d0795223570719cacd2498f86a35
Collins Omwaga <collinsomwaga@gmail.com>
2019-06-30 16:19:30 +0300

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AddComponent } from './pages/add/add.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { AddCardComponent } from './ui/add-card/add-card.component';
+import { WeatherCardComponent } from './ui/weather-card/weather-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddComponent,
+    DetailsComponent,
+    LoginComponent,
+    SignupComponent,
+    AddCardComponent,
+    WeatherCardComponent
   ],
   imports: [
     BrowserModule,
